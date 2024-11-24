c8a67089f8b52ae257ebdc702257c9048693e961
The Teleporter <m6253272@gmail.com>
2024-11-24 19:14:45 +0300

    Update opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,18 +1,18 @@
 import { ImageResponse } from 'next/og';
 
-async function loadGoogleFont (font: string, text: string) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}:wght@900&text=${encodeURIComponent(text)}`
-  const css = await (await fetch(url)).text()
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+async function loadGoogleFont(font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}:wght@900&text=${encodeURIComponent(text)}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(/src: url(.+) format'(opentype|truetype)'/);
 
   if (resource) {
-    const response = await fetch(resource[1])
-    if (response.status == 200) {
-      return await response.arrayBuffer()
+    const response = await fetch(resource[1]);
+    if (response.status === 200) {
+      return await response.arrayBuffer();
     }
   }
 
-  throw new Error('failed to load font data')
+  throw new Error('Failed to load font data');
 }
 
 export async function GET() {
@@ -20,31 +20,31 @@ export async function GET() {
 
   return new ImageResponse(
     (
- <div
-  style={{
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#000',
-    fontSize: 32,
-    fontWeight: 600,
-     color: '#fff',
-  }}
->
-  <svg
-    width="75"
-    height="75"
-    viewBox="0 0 75 75"
-    fill="#fff"
-    style={{ margin: '0 75px 3px' }}
-  >
-    <rect x="0" y="0" width="75" height="75" />
-  </svg>
-  <div style={{ marginTop: 40 }}>{text}</div>
-</div>
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: '#000',
+          fontSize: 32,
+          fontWeight: 600,
+          color: '#fff',
+        }}
+      >
+        <svg
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          fill="#fff"
+          style={{ margin: '0 75px 3px' }}
+        >
+          <rect x="0" y="0" width="75" height="75" />
+        </svg>
+        <div style={{ marginTop: 40 }}>{text}</div>
+      </div>
     ),
     {
       width: 800,
