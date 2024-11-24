d6df55d6a895d625366a4ba208319d4b5fd5712b
The Teleporter <m6253272@gmail.com>
2024-11-24 15:16:34 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+       tapHighlightColor: {
+        initial: 'initial',
+      },
     },
   },
   plugins: [],
