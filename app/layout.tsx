d59e0e261453e7be18adeb70228599ea3f658cf1
The Teleporter <m6253272@gmail.com>
2024-11-24 19:33:38 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,21 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Black Labs by The Teleporter",
   description: "Welcome to my labs. Here you\'ll find experiments and work I\'ve done. I love designing and drawing things so I just explore ideas that will give me advanced knowledge in certain concepts.",
+openGraph: {
+    title: 'Black Labs by The Teleporter',
+    description: 'Black Labs' is a studio lab that showcases crafts and experiments done by The Teleporter while exploring various fields in the web eco system.',
+    url: 'https://lab.theteleporter.me',
+    siteName: 'Black Labs by The Teleporter',
+    images: [
+      {
+        url: 'https://lab.theteleporter.me/api/og',
+        width: 800,
+        height: 400,
+      },
+     ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
