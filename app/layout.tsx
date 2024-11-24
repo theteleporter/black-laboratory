0261aae99e24feaee37e8c8bdac26f39f64c1669
The Teleporter <m6253272@gmail.com>
2024-11-24 14:11:49 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono font-thin`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono font-[100]`}
       >
         {children}
       </body>
