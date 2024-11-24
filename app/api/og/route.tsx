984b5198d39b527cc2a31e6a607dab6ece49961d
The Teleporter <m6253272@gmail.com>
2024-11-24 18:11:52 +0300

    Update route.tsx

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,6 +1,23 @@
 import { ImageResponse } from 'next/og';
  
+async function loadGoogleFont (font: string, text: string) {
+  const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
+  const css = await (await fetch(url)).text()
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+ 
+  if (resource) {
+    const response = await fetch(resource[1])
+    if (response.status == 200) {
+      return await response.arrayBuffer()
+    }
+  }
+ 
+  throw new Error('failed to load font data')
+}
+ 
 export async function GET() {
+  const text = 'Black Labs';
+ 
   return new ImageResponse(
     (
  <div
@@ -26,12 +43,19 @@ export async function GET() {
   >
     <rect x="0" y="0" width="75" height="75" />
   </svg>
-  <div style={{ marginTop: 40 }}>Black Labs</div>
+  <div style={{ marginTop: 40 }}>{text}</div>
 </div>
     ),
     {
       width: 800,
       height: 400,
+      fonts: [
+        {
+          name: 'Geist',
+          data: await loadGoogleFont('Geist', text),
+          style: 'mono',
+        },
+      ],
     },
   );
 }
