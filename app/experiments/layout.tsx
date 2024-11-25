b08e87c05376426c012233c7467e3ffe0ad68c8d
The Teleporter <m6253272@gmail.com>
2024-11-25 19:37:48 +0300

    Update layout.tsx

--- app/experiments/layout.tsx
+++ app/experiments/layout.tsx
@@ -16,22 +16,22 @@ export default function ExperimentLayout({
  // const hasSourceCode = true
 
   return (
-    <div className="min-h-screen bg-[#161616] text-stone-200 p-4 font-mono relative">
+    <div className="min-h-screen text-stone-200 p-4 font-mono relative z-10">
       <Link 
         href="/" 
-        className="absolute top-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200"
+        className="absolute top-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 z-40"
       >
         <ArrowLeft size={24} />
         <span className="sr-only">Back to Lab</span>
       </Link>
       
-      <main className="pt-16 pb-16">
+      <main className="-z-10">
         {children}
       </main>
 
         <Link 
           href={`https://github.com/theteleporter/lab`} 
-          className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2"
+          className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
         >
           <Code size={24} />
          {/** <span className="text-sm">Source Code</span>**/}
