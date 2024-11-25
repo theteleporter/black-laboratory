9acfbf896a91c4a484b791f0d01dde062148f935
The Teleporter <m6253272@gmail.com>
2024-11-25 16:25:27 +0000

    experiments layout fixed

--- app/experiments/[experiment]/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ExperimentPage({ params }: { params: { experiment: string } }) {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-8rem)]">
-      <h1 className="text-2xl font-light mb-4">{params.experiment.toUpperCase()}</h1>
-      <p className="text-stone-400">Experiment content goes here</p>
-    </div>
-  )
-}
-
