a0853fd9f387577068136db191b24433d8014104
The Teleporter <m6253272@gmail.com>
2024-11-25 14:58:01 +0300

    Update page.tsx

--- app/experiments/command-card/page.tsx
+++ app/experiments/command-card/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react'
-import { Card } from "../../../components/ui/card"
+import { Card } from "../../../components/card"
 import { Download } from "lucide-react"
 import { toPng } from 'html-to-image'
 import { saveAs } from 'file-saver'
