4e5801426cae5414cf480ef1f6b1fe09730ea222
The Teleporter <m6253272@gmail.com>
2024-11-26 13:19:51 +0300

    Update layout.tsx

--- app/experiments/layout.tsx
+++ app/experiments/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import Link from 'next/link'
-import { ArrowLeft, Code } from 'lucide-react'
+import { Code } from 'lucide-react'
 import BackButton from '../../components/buttons'
 
 export const metadata: Metadata = {
@@ -9,34 +9,33 @@ export const metadata: Metadata = {
 }
 
 export default function ExperimentLayout({
-  children
+  children,
+  source
 }: {
   children: React.ReactNode
+  source: string
 }) {
-  // This is a placeholder. In a real app, you'd determine this based on the actual experiment.
- // const hasSourceCode = true
-
   return (
     <div className="min-h-screen text-stone-200 font-mono relative z-10">
       <Link 
         href="/" 
         className="absolute top-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 z-40"
       >
-         <BackButton variant="dark" />
+        <BackButton variant="dark" />
         <span className="sr-only">Back to Lab</span>
       </Link>
-      
+
       <main className="-z-10">
         {children}
       </main>
 
-        <Link 
-          href={`https://github.com/theteleporter/lab`} 
-          className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
-        >
-          <Code size={24} />
-         <span className="sr-only">Source Code</span>
-        </Link>
+      <Link 
+        href={source} 
+        className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
+      >
+        <Code size={24} />
+        <span className="sr-only">Source Code</span>
+      </Link>
     </div>
   )
 }
