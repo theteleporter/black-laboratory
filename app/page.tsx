b0d9a6c1a01d73fc7fa3d25001a76ae98e394392
The Teleporter <m6253272@gmail.com>
2024-11-26 06:49:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import ProjectList from '../components/project-list'
-import Footer from '../components/footer'
+import ServerFooter from '../components/server-footer'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#161616] text-stone-200 p-4 font-mono">
       <h1 className="text-xl mb-8 font-light">BLACK LABS</h1>
       <ProjectList />
-      <Footer />
+      <ServerFooter />
     </main>
   )
 }
