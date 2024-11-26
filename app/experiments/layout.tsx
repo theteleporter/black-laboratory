60ff559afd473d30fe6e00bdc8ccd8e954198730
The Teleporter <m6253272@gmail.com>
2024-11-26 18:05:56 +0300

    Update layout.tsx

--- app/experiments/layout.tsx
+++ app/experiments/layout.tsx
@@ -1,18 +1,25 @@
-import type { Metadata } from 'next'
-import Link from 'next/link'
-import { Code } from 'lucide-react'
-import BackButton from '../../components/buttons'
+import type { Metadata } from 'next';
+import Link from 'next/link';
+import { Code } from 'lucide-react';
+import BackButton from '../../components/buttons';
+import { getExperiments } from '../../utils/getExperiments';
 
 export const metadata: Metadata = {
   title: 'Black Labs | Experiment',
   description: 'Explore some experiments done on the web using some things and other things.',
-}
+};
 
 export default function ExperimentLayout({
   children,
+  params, // Use dynamic route params to determine the experiment
 }: {
   children: React.ReactNode;
+  params: { experiment: string };
 }) {
+  const experiments = getExperiments();
+  const currentExperiment = experiments.find((exp) => exp.name === params.experiment);
+  const sourceLink = currentExperiment?.sourceLink;
+
   return (
     <div className="min-h-screen text-stone-200 font-mono relative z-10">
       <Link 
@@ -23,18 +30,17 @@ export default function ExperimentLayout({
         <span className="sr-only">Back to Lab</span>
       </Link>
 
-      <main className="-z-10">
-        {children}
-      </main>
+      <main className="-z-10">{children}</main>
 
-      <Link 
-        href="https://github.com/theteleporter/lab"
-        className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
-      >
-        <Code size={24} />
-        <span className="sr-only">Source Code</span>
-      </Link>
+      {sourceLink && (
+        <Link 
+          href={sourceLink} 
+          className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
+        >
+          <Code size={24} />
+          <span className="sr-only">Source Code</span>
+        </Link>
+      )}
     </div>
-  )
+  );
 }
-
