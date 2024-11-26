7d3d49f83aef03fbf9c2092d91d461f366861fb3
The Teleporter <m6253272@gmail.com>
2024-11-26 13:15:46 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from 'next/script';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,8 +16,8 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "Black Labs by The Teleporter",
-  description: "Welcome to my labs. Here you\'ll find experiments and work I\'ve done. I love designing and drawing things so I just explore ideas that will give me advanced knowledge in certain concepts.",
-openGraph: {
+  description: "Welcome to my labs. Here you'll find experiments and work I've done. I love designing and drawing things so I just explore ideas that will give me advanced knowledge in certain concepts.",
+  openGraph: {
     title: 'Black Labs by The Teleporter',
     description: 'Black Labs is a studio lab that showcases crafts and experiments done by The Teleporter while exploring various fields in the web eco system.',
     url: 'https://lab.theteleporter.me',
@@ -27,10 +28,10 @@ openGraph: {
         width: 800,
         height: 400,
       },
-     ],
-  locale: 'en_US',
-  type: 'website',
- },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -40,6 +41,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script id="json-ld" type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "Black Labs by The Teleporter",
+            "url": "https://lab.theteleporter.me",
+            "description": "Experiments in design and technology",
+            "author": {
+              "@type": "Person",
+              "name": "The Teleporter"
+            }
+          })}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono font-thin`}
       >
@@ -48,3 +64,4 @@ export default function RootLayout({
     </html>
   );
 }
+
