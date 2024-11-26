7d305e4133229d7c3fb17c110379e2388b33e9e3
The Teleporter <m6253272@gmail.com>
2024-11-26 18:05:21 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,18 @@
-import { MetadataRoute } from 'next'
-import { getExperiments } from '../utils/getExperiments'
+import { MetadataRoute } from 'next';
+import { getExperiments } from '../utils/getExperiments';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const experiments = getExperiments()
-  const baseUrl = 'https://lab.theteleporter.me'
+  const experiments = getExperiments();
+  const baseUrl = 'https://lab.theteleporter.me';
 
   return [
     {
       url: baseUrl,
       lastModified: new Date(),
     },
-    ...experiments.map(experiment => ({
-      url: `${baseUrl}/experiments/${experiment}`,
+    ...experiments.map(({ name }) => ({
+      url: `${baseUrl}/experiments/${name}`, // Use the `name` property from each experiment
       lastModified: new Date(),
-    }))
-  ]
+    })),
+  ];
 }
-
