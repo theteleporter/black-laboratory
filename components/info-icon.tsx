66885d47256fe63a3b58eb50547ef5be0fcdf55b
The Teleporter <m6253272@gmail.com>
2024-11-28 02:24:36 +0300

    Update info-icon.tsx

--- components/info-icon.tsx
+++ components/info-icon.tsx
@@ -67,6 +67,7 @@ const InfoIcon = ({ tooltip, variant = "default", side = "top" }: InfoIconProps)
               <path d="M12 16v-4" />
               <path d="M12 8h.01" />
             </svg>
+          <span className="sr-only">More Information<span>
           </button>
         </TooltipTrigger>
         <TooltipContent
