b10f06244e80df5d01ba75657d28a51e3d0b3d81
The Teleporter <m6253272@gmail.com>
2024-11-27 15:40:43 +0300

    Update info-icon.tsx

--- components/info-icon.tsx
+++ components/info-icon.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import * as React, { useState } from "react"
+import * as React from "react"
+import { useState } from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 interface InfoIconProps {
