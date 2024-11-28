1cc385e7b315433feec6e6cd39e2a7830a0176d3
The Teleporter <m6253272@gmail.com>
2024-11-28 04:07:43 +0300

    Update page.tsx

--- app/experiments/dynamic-avatar/page.tsx
+++ app/experiments/dynamic-avatar/page.tsx
@@ -1,15 +1,17 @@
-import type { Metadata } from 'next'
+import { Metadata } from 'next'
 import Component from './Component'
+import InfoIcon from '../../../components/info-icon'
+import { Code } from 'lucide-react'
 
 export const metadata: Metadata = {
   title: 'Dynamic Avatar | Black Labs',
-  description: 'An experiment that generates a dynamic avatar based on email input.',
+  description: 'An experiment generating a dynamic avatar based on text input. The avatar’s gradient colors change according to every keystroke.',
   openGraph: {
     title: 'Dynamic Avatar | Black Labs',
-    description: 'An experiment that generates a dynamic avatar based on email input.',
+    description: 'An experiment generating a dynamic avatar based on text input. The avatar’s gradient colors change according to every keystroke.',
     images: [
       {
-        url: '/api/og?experiment=dynamic-avatar',
+        url: '/api/og',
         width: 800,
         height: 400,
       },
@@ -18,6 +20,27 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <Component />
-}
+  const description = metadata.description ?? "No description available."
 
+  return (
+    <>
+      <div className="absolute top-4 right-4 z-40">
+        <InfoIcon 
+          tooltip={description} 
+          variant="default" 
+          side="bottom" 
+        />
+      </div>
+      <Component />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/theteleporter/lab/tree/main/app/experiments/dynamic-avatar"
+        className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
+      >
+        <Code size={20} />
+        <span className="sr-only">Source Code</span>
+      </a>
+    </>
+  )
+}
