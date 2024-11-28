b314082467cfeeb0dc1ca56eab3fdc7b10acfc91
The Teleporter <m6253272@gmail.com>
2024-11-28 04:31:13 +0300

    Update page.tsx

--- app/experiments/logo-experiments/page.tsx
+++ app/experiments/logo-experiments/page.tsx
@@ -1,15 +1,19 @@
-import type { Metadata } from 'next'
+"use client"
+
+import { Metadata } from 'next'
 import Component from './Component'
+import InfoIcon from '../../../components/info-icon'
+import { Code } from 'lucide-react'
 
 export const metadata: Metadata = {
   title: 'Logo Experiments | Black Labs',
-  description: 'An experiment showcasing logos and a content editable field that can be used to generate logos on the fly. Also custom fonts are also supported. ',
+  description: 'An experiment showcasing logos and a content editable field that can be used to generate logos on the fly. Custom fonts are also supported.',
   openGraph: {
     title: 'Logo Experiments | Black Labs',
-    description: 'An experiment showcasing logos and a content editable field that can be used to generate logos on the fly. Also custom fonts are also supported .',
+    description: 'An experiment showcasing logos and a content editable field that can be used to generate logos on the fly. Custom fonts are also supported.',
     images: [
       {
-        url: '/api/og?experiment=logo-experiments',
+        url: '/api/og',
         width: 800,
         height: 400,
       },
@@ -18,5 +22,27 @@ export const metadata: Metadata = {
 }
 
 export default function Page() {
-  return <Component />
+  const description = metadata.description ?? "No description available."
+
+  return (
+    <>
+      <div className="absolute top-4 right-4 z-40">
+        <InfoIcon 
+          tooltip={description} 
+          variant="default" 
+          side="bottom" 
+        />
+      </div>
+      <Component />
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/theteleporter/lab/tree/main/app/experiments/logo-experiments"
+        className="absolute bottom-4 left-4 text-stone-400 hover:text-stone-200 transition-colors duration-200 flex items-center gap-2 z-40"
+      >
+        <Code size={20} />
+        <span className="sr-only">Source Code</span>
+      </a>
+    </>
+  )
 }
