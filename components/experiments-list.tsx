5e3b55695e307f13cba0dd6e1a3eaeb3fca6c24e
theteleporter <m6253272@gmail.com>
2024-12-01 18:12:11 +0300

    update experiments-list.tsx

--- components/experiments-list.tsx
+++ components/experiments-list.tsx
@@ -201,7 +201,7 @@ export default function ProjectList() {
               }}
               className="absolute right-0 z-10 bg-gradient-to-l from-[#161616] to-transparent pl-4 h-full flex items-center"
             >
-            <span className='sr-only'>Previous</span>
+            <span className='sr-only'>Next</span>
               <ChevronRight size={16} className="text-stone-400" />
             </button>
           </div>
@@ -223,7 +223,7 @@ export default function ProjectList() {
               disabled={currentPage === totalPages}
               className="text-stone-400 hover:text-white disabled:opacity-50"
             >
-           <span className='sr-only'></span>
+           <span className='sr-only'>Next</span>
               <ChevronRight size={20} />
             </button>
           </div>
