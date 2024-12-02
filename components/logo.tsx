e9de8e0907e2afa90375722469ac93adb7cc6dd0
theteleporter <m6253272@gmail.com>
2024-12-02 09:08:39 +0300

    update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,21 +1,44 @@
-export const Logo = () => {
- return (
-  <h1 className="font-dela-gothic-one text-xl select-none">BLACK LABS</h1>
- )
+import React from 'react'
+
+export const Logo: React.FC = () => {
+  return (
+    <svg
+      width="220"
+      height="40"
+      viewBox="0 0 220 35"
+      fill="red"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <style>
+        {`
+          @import url('https://fonts.googleapis.com/css2?family=Dela+Gothic+One&display=swap');
+        `}
+      </style>
+      <text
+        x="10"
+        y="35"
+        fontFamily="'Dela Gothic One', sans"
+        fontSize="24"
+        fill="currentColor"
+        className="select-none"
+      >
+        BLACK LABS
+      </text>
+    </svg>
+  )
 }
 
-export const Icon = () => {
- return (
-  <div className="font-dela-gothic-one text-xl">
-  <svg
-    width="75"
-    height="75"
-    viewBox="0 0 75 75"
-    fill="#fff"
- // style={{ margin: '0 75px 3px' }}
+
+export const Icon: React.FC = () => {
+  return (
+    <svg
+      width="75"
+      height="75"
+      viewBox="0 0 75 75"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-   <rect x="0" y="0" width="75" height="75" />
-   </svg>
-  </div>
- )
+      <rect width="75" height="75" fill="#000" />
+    </svg>
+  )
 }
