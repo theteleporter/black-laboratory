0ae682f922baee0fe5a57e0a6f31d1953ea68224
The Teleporter <m6253272@gmail.com>
2025-01-26 12:26:17 +0300

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://lab.theteleporter.me/sitemap.xml',
+    sitemap: 'https://blacklabs.vercel.app/sitemap.xml',
   }
 }
